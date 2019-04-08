4d2bea574b75f7b16b5ca1ab1bba4fbb65502a33
Yash Naik <36054936+Naik-yash@users.noreply.github.com>
2019-04-08 15:18:05 -0400

    Manually Coding Regression
    1. Formula of slope
    2. Formula of Regression line 
    3. Visualization
    4. Prediction
    5. Squared Error
    6. Coefficient of determination

---
+++ Manually coding regression.py
@@ -0,0 +1,57 @@
+from statistics import mean
+import numpy as np
+xs = [1,2,3,4,5]
+ys = [5,4,6,5,6]
+xs = np.array([1,2,3,4,5], dtype=np.float64)
+ys = np.array([5,4,6,5,6], dtype=np.float64)
+
+#slope 
+
+def best_fit_slope_and_intercept(xs,ys):
+    m = (((mean(xs)*mean(ys)) - mean(xs*ys)) /
+         ((mean(xs)*mean(xs)) - mean(xs*xs)))
+    
+    b = mean(ys) - m*mean(xs)
+    
+    return m, b
+
+m,b = best_fit_slope(xs,ys)
+print(m,b)
+
+#regression line -[(m*x)+b for x in xs]
+regression_line = []
+for x in xs:
+    regression_line.append((m*x)+b)
+
+#visualization
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+plt.scatter(xs,ys,color='#003F72')
+plt.plot(xs, regression_line)
+plt.show()
+
+
+#prediction
+predict_x = 7
+predict_y = (m*predict_x)+b
+print(predict_y)
+plt.scatter(xs,ys,color='#003F72',label='data')
+plt.plot(xs, regression_line, label='regression line')
+plt.legend(loc=4)
+plt.show()
+
+#squaredd error 
+def squared_error(ys_orig,ys_line):
+    return sum((ys_line - ys_orig) * (ys_line - ys_orig))
+
+#coefficient of determination
+def coefficient_of_determination(ys_orig,ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    return 1 - (squared_error_regr/squared_error_y_mean)
+
+
+r_squared = coefficient_of_determination(ys,regression_line)
+print(r_squared)
